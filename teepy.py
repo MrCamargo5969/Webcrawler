1ed94a4bfe814dbfac9925d42fa3ef84d45dd64f
mateus-robers-amaral <160862454+mateus-robers-amaral@users.noreply.github.com>
2024-04-22 17:12:49 -0300

    Update teepy.py

--- teepy.py
+++ teepy.py
@@ -17,13 +17,19 @@ url = 'https://twitter.com'
 
 web.get(url)
 
-web.find_element('xpath', '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/a').click()
+web.find_element('xpath', '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a').click()
 time.sleep(2)
 
-web.find_element('xpath', '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').click()
+web.find_element('xpath', '///*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').click()
 time.sleep(2)
 
-web.find_element('xpath', '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[5]/a').send_keys('pescdata@gmail.com')
+web.find_element('xpath', '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').send_keys('pescdata@gmail.com')
+time.sleep(2)
+
+web.find_element('xpath', '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div/span/span').click()
+time.sleep(2)
+
+web.find_element('xpath', '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input').send_keys('')
 time.sleep(2)
 
 itens = web.find_elements(By.CLASS_NAME, 'ui-search-item__title')
